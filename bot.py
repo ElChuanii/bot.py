04d568b5962c7455d86bd9f55ae678458ed7d560
Juani <juanignaciolagomarsino@gmail.com>
2026-04-14 20:19:59 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,30 @@
+import discord
+
+from bot_logic import gen_pass
+
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable cliente y transferirle los privilegios
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('$pass'):
+        password = gen_pass(10)
+        await message.channel.send(f"Tu contraseña es: {password}")   
+    else:
+        await message.channel.send(message.content)
+
+client.run("")
